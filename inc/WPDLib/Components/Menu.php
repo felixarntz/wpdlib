0a6de9c749f63ed2b945ac4f3d226b51dadd68ad
Felix <felix-arntz@t-online.de>
2015-08-19 20:22:18 +0200

    added add_*_page mappings to menu

--- inc/WPDLib/Components/Menu.php
+++ inc/WPDLib/Components/Menu.php
@@ -41,14 +41,56 @@ if ( ! class_exists( 'WPDLib\Components\Menu' ) ) {
 				// check for the sanitized menu title
 				$this->added = true;
 				$this->menu_slug = $key;
-			} elseif ( ! in_array( $this->slug, array( 'menu', 'submenu', 'object' ) ) && function_exists( 'add_' . $this->slug . '_page' ) ) {
+			} elseif ( ! in_array( $this->slug, array( 'menu', 'submenu', 'object', 'utility' ) ) && function_exists( 'add_' . $this->slug . '_page' ) ) {
 				// check for submenu page function
 				$this->added = true;
+				switch ( $this->slug ) {
+					case 'posts':
+						$this->menu_slug = 'edit.php';
+						break;
+					case 'media':
+						$this->menu_slug = 'upload.php';
+						break;
+					case 'links':
+						$this->menu_slug = 'link-manager.php';
+						break;
+					case 'pages':
+						$this->menu_slug = 'edit.php?post_type=page';
+						break;
+					case 'comments':
+						$this->menu_slug = 'edit-comments.php';
+						break;
+					case 'management':
+						$this->menu_slug = 'tools.php';
+						break;
+					case 'theme':
+						$this->menu_slug = 'themes.php';
+						break;
+					case 'plugins':
+						$this->menu_slug = 'plugins.php';
+						break;
+					case 'users':
+						if ( current_user_can( 'edit_users' ) ) {
+							$this->menu_slug = 'users.php';
+						} else {
+							$this->menu_slug = 'profile.php';
+						}
+						break;
+					case 'dashboard':
+						$this->menu_slug = 'index.php';
+						break;
+					case 'options':
+					default:
+						$this->menu_slug = 'options-general.php';
+				}
 				$this->menu_slug = 'add_' . $this->slug . '_page';
 			} elseif ( isset( $admin_page_hooks[ 'edit.php?post_type=' . $this->slug ] ) ) {
 				// check if it is a post type menu
 				$this->added = true;
 				$this->menu_slug = 'edit.php?post_type=' . $this->slug;
+			} elseif ( 'link' == $this->slug ) {
+				$this->added = true;
+				$this->menu_slug = 'link-manager.php';
 			} elseif ( 'attachment' == $this->slug ) {
 				$this->added = true;
 				$this->menu_slug = 'upload.php';
