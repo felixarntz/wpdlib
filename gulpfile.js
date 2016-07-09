65d3029b2b377dc68d418a2d4ff65c2b9b0db838
Felix Arntz <felix-arntz@t-online.de>
2016-07-09 14:15:53 +0200

    ready for 0.6.5

--- gulpfile.js
+++ gulpfile.js
@@ -64,7 +64,7 @@ var paths = {
 /* ---- MAIN TASKS ---- */
 
 // general task (compile Sass and JavaScript and refresh POT file)
-gulp.task( 'default', [ 'sass', 'js' ]);
+gulp.task( 'default', [ 'sass', 'js', 'pot' ]);
 
 // watch Sass and JavaScript files
 gulp.task( 'watch', function() {
